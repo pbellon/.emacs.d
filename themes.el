129ec884cc2b28916486647767bf5dcfe88ccc2d
Pierre BELLON <bellon.pierre@gmail.com>
2019-08-22 12:10:17 +0200

    Fix themes conf

--- themes.el
+++ themes.el
@@ -1,10 +1,10 @@
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'monokai t)
 
 (use-package smart-mode-line
   :ensure t
   :config
   :init
-  (load-theme 'monokai t)
-  (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
   (setq sml/no-confirm-load-theme t)
   (setq sml/theme 'respectful)
   :config
