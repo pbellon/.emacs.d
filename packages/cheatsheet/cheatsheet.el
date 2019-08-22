445c872c22f4f24d8ae96da3016dd23b7c4a6aa2
Pierre Bellon <bellon.pierre@gmail.com>
2019-08-22 23:02:21 +0200

    Almost working cheatsheet package

--- packages/cheatsheet/cheatsheet.el
+++ packages/cheatsheet/cheatsheet.el
@@ -1,25 +1,22 @@
-
-(defvar cheat/registry (make-hash-table :test 'equal))
-
-(defun cheat/open (name))
-
-
-(defun cheat/register (name fname)
-  "Register a new cheatsheet, will create cheat/<name> interactive function"
-  (puthash name fname cheat/registry)
-  (eval `(defun cheat/,name ()
-           (interactive)
-           (cheat/open ',name))))
+(defconst cheat/sheets '((adoc "Asciidoc" "./sheets/asciidoc.org"))
 )
 
-(defun cheat/setup ()
-  )
+(defun cheat/open (wname fname)
+  (let ((buffer (generate-new-buffer (concat "* " wname " Cheatsheet *"))))
+    (set-buffer-major-mode buffer)
+    (org-mode)
+    (display-buffer buffer '(display-buffer-pop-up-frame . nil))))
 
+(defun cheat/register (cheat-name cheat-wname cheat-fname)
+  "Register a new cheatsheet, will create cheat/<name> interactive function"
+  (let ((cheat-fn-name (intern (format "cheat/%s" cheat-name))))
+    `(defun ,cheat-fn-name ()
+       (interactive)
+       (cheat/open ,cheat-wname ,cheat-fname))))
 
-(defun test (name)
-  (eval `(defun ,name ()
-           (interactive)
-           (message "It works"))
-        ))
+(defmacro cheat/setup ()
+  `(progn ,@(mapcar
+             (lambda (x) (cheat/register (nth 0 x) (nth 1 x) (nth 2 x)))
+             cheat/sheets)))
 
-(test 'toto)
+(cheat/setup)
