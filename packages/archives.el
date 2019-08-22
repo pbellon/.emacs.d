f3160cd225f2c4129973fe7cb721ed62fe33695a
Pierre Bellon <bellon.pierre@gmail.com>
2019-08-22 23:01:59 +0200

    Removes packages/archives.el

--- packages/archives.el
+++
@@ -1,8 +0,0 @@
-(add-to-list 'package-archives
-  '("gnu" . "https://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives
-  '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives
-  '("org" . "https://orgmode.org/elpa/"))
-
-(package-initialize)
