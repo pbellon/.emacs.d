141a716f2ed5ec68ef264a02f0ef81e77e22805d
Pierre Bellon <bellon.pierre@gmail.com>
2019-08-24 23:42:57 +0200

    Rename cheatsheet to cheat & make it work

--- packages/cheat/cheat.el
+++ packages/cheat/cheat.el
@@ -2,10 +2,11 @@
 )
 
 (defun cheat/open (wname fname)
-  (let ((buffer (generate-new-buffer (concat "* " wname " Cheatsheet *"))))
-    (set-buffer-major-mode buffer)
-    (org-mode)
-    (display-buffer buffer '(display-buffer-pop-up-frame . nil))))
+  (let (($b (generate-new-buffer (concat "* " wname " Cheatsheet *"))))
+    (set-buffer-major-mode $b)
+    (switch-to-buffer $b)
+    (insert-file-contents fname)
+    (org-mode)))
 
 (defun cheat/register (cheat-name cheat-wname cheat-fname)
   "Register a new cheatsheet, will create cheat/<name> interactive function"
@@ -14,9 +15,15 @@
        (interactive)
        (cheat/open ,cheat-wname ,cheat-fname))))
 
-(defmacro cheat/setup ()
+(defmacro cheat/init ()
   `(progn ,@(mapcar
-             (lambda (x) (cheat/register (nth 0 x) (nth 1 x) (nth 2 x)))
+             (lambda (x)
+               (cheat/register (nth 0 x) (nth 1 x) (nth 2 x)))
              cheat/sheets)))
 
-(cheat/setup)
+(defun cheat/setup ()
+  "Initialize all cheatsheets"
+  (interactive)
+  (cheat/init))
+
+(provide 'cheat)
