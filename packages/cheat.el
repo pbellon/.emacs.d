01a5ca8ae0ffa06223938cc5f11ed87d231e55f3
Pierre BELLON <bellon.pierre@gmail.com>
2019-08-28 13:51:59 +0200

    Update repo url of cheat.el

--- packages/cheat.el
+++ packages/cheat.el
@@ -1,5 +1,5 @@
 (use-package cheat
-  :quelpa (cheat :fetcher git :url "https://framagit.org/pbellon/cheat.git")
+  :quelpa (cheat :fetcher git :url "https://github.com/pbellon/cheat.git")
   :config
   (add-to-list 'cheat/sheets
                '(kbd "Custom Keyboard shortcuts" "~/.emacs/cheatsheets/kbd.org"))
