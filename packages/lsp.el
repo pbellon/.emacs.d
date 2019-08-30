4928b07951a8e695f7a8ef9a3519d532ca94af6d
Pierre BELLON <bellon.pierre@gmail.com>
2019-08-30 20:30:36 +0200

    Removed lsp.el

--- packages/lsp.el
+++
@@ -1,8 +0,0 @@
-(use-package lsp-mode
-  :ensure t
-  :hook ((javascript-mode typescript-mode) . lsp)
-  :init
-  ;; TODO: communiquer avec SonarLint-Core via son Language Server.
-  ;; (lsp-register-client
-  ;;  (make-lsp-client :new-connection (lsp-tramp-connection "binary-or-full-path" )))
-  )
