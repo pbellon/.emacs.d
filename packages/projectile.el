ee03b17c369315a9c6e0f139474042c43778d85d
Pierre BELLON <bellon.pierre@gmail.com>
2019-09-02 09:49:49 +0200

    Adds new projet type to projectile (NPM)

--- packages/projectile.el
+++ packages/projectile.el
@@ -1,6 +1,15 @@
 (use-package projectile
   :ensure t
   :config
+  ;; Adds npm as a new project type
+  (projectile-register-project-type 
+   'npm 
+   '("package.json")
+   :compile "npm install"
+   :test "npm test"
+   :run "npm start"
+   :test-suffix ".spec")
+
   (setq projectile-project-search-path '("~/Dev"))
   (setq projectile-enable-caching t)
   (setq projectile-globally-ignored-directories
