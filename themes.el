5886587bd7ac81769a714f019a61f2f9870ad3de
Pierre BELLON <bellon.pierre@gmail.com>
2019-09-04 00:22:05 +0200

    Adds doom-modeline & easier way to switch between one-themes light & dark & reload-theme function

--- themes.el
+++ themes.el
@@ -1,16 +1,38 @@
+;;; global theme
+(defcustom use-light-theme nil
+  "Check to use the light theme"
+  :type 'bool
+  :group 'theme
+)
+(setq use-light-theme nil)
+
+(defun reload-theme ()
+  "Forces emacs to reload the configured theme"
+  (interactive)
+  (if (not (eq use-light-theme nil))
+    (load-theme 'one-light t)
+    (load-theme 'one-dark t)
+  )
+)
+
 (use-package one-themes
   :ensure t
   :init
-  (load-theme 'one-dark t))
+  (reload-theme))
 
+;;; mode lines
 
-(use-package smart-mode-line
-  :after one-themes
+;; (use-package smart-mode-line
+;;   :after one-themes
+;;   :ensure t
+;;   :init
+;;   (setq sml/no-confirm-load-theme t)
+;;   (setq sml/theme 'respectful)
+;;   :config
+;;   (sml/setup)
+;; )
+
+(use-package doom-modeline
   :ensure t
-  :init
-  (setq sml/no-confirm-load-theme t)
-  (setq sml/theme 'respectful)
   :config
-  (sml/setup)
-)
-
+  (doom-modeline-mode 1))
