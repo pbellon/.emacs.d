cbdd662b3d9918d278f4191f9c77f31df630f7bf
Pierre BELLON <bellon.pierre@gmail.com>
2019-09-03 18:06:01 +0200

    Put back old cheat configuration

--- packages/cheat.el
+++ packages/cheat.el
@@ -1,20 +1,15 @@
-;; (use-package cheat
-;;   :quelpa 
-;;   (cheat
-;;     :fetcher github
-;;     :repo "pbellon/cheat"
-;;     :files 
-;;     ("*.el" "*.el.in" "sheets" "sheets/*.org"
-;;      "*.info" "*.texi" "*.texinfo"
-;;      (:exclude ".dir-locals.el" "test.el" "tests.el" "*-test.el" "*-tests.el")))
-;;   :ensure t
-;;   ;; :quelpa (cheat :fetcher git :url "https://github.com/pbellon/cheat.git")
-;;   :config
-;;   (add-to-list 'cheat/sheets-folders "~/.emacs.d/cheatsheets")
-;;   (cheat/init)
-;; )
-(add-to-list 'load-path "~/Dev/cheat")
-(require 'cheat)
-(with-eval-after-load 'cheat
+(use-package cheat
+  :quelpa 
+  (cheat
+    :fetcher github
+    :repo "pbellon/cheat"
+    :files 
+    ("*.el" "*.el.in" "sheets" "sheets/*.org"
+     "*.info" "*.texi" "*.texinfo"
+     (:exclude ".dir-locals.el" "test.el" "tests.el" "*-test.el" "*-tests.el")))
+  :ensure t
+  ;; :quelpa (cheat :fetcher git :url "https://github.com/pbellon/cheat.git")
+  :config
   (add-to-list 'cheat/sheets-folders "~/.emacs.d/cheatsheets")
-  (cheat/reload-sheets))
+  (cheat/reload-sheets)
+)
