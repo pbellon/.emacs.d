f78ed54433fe3e3f213ff4db3248ac5c96e3448b
Pierre BELLON <bellon.pierre@gmail.com>
2019-09-03 23:17:11 +0200

    Updates cheat conf

--- packages/cheat.el
+++ packages/cheat.el
@@ -1,15 +1,7 @@
 (use-package cheat
-  :quelpa 
-  (cheat
-    :fetcher github
-    :repo "pbellon/cheat"
-    :files 
-    ("*.el" "*.el.in" "sheets" "sheets/*.org"
-     "*.info" "*.texi" "*.texinfo"
-     (:exclude ".dir-locals.el" "test.el" "tests.el" "*-test.el" "*-tests.el")))
   :ensure t
-  ;; :quelpa (cheat :fetcher git :url "https://github.com/pbellon/cheat.git")
   :config
   (add-to-list 'cheat/sheets-folders "~/.emacs.d/cheatsheets")
   (cheat/reload-sheets)
+  (cheat--macro)
 )
