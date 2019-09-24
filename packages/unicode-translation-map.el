a8e473f6e635b4f1b4f5896288578c0d311110f5
Pierre BELLON <bellon.pierre@gmail.com>
2019-09-24 12:17:29 +0200

    Adds unicode translation map definitions

---
+++ packages/unicode-translation-map.el
@@ -0,0 +1,4 @@
+;;; Defines list of kbd translations for special unicode chars.
+
+(define-key key-translation-map (kbd "<f9> p") (kbd "φ"))
+(define-key key-translation-map (kbd "<f9> d") (kbd "Δ"))
