80ebfcae4a377215a0e54a5149a6d197baedee3c
Pierre BELLON <pbellon@cirilgroup.com>
2019-10-01 13:56:58 +0200

    Change back to js2-mode (better integration with imenu-list)

--- packages/js.el
+++ packages/js.el
@@ -1,4 +1,6 @@
-(use-package js
+(use-package js2-mode
+  :after (flycheck)
   :ensure t
+  :hook (js2-mode . flycheck-mode)
   :config
-  (add-hook 'js-mode-hook #'flycheck-mode)) 
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
