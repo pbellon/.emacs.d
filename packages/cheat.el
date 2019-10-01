bf2db35d00be4f63fe7a32986200614e9f0e89b7
Pierre BELLON <pbellon@cirilgroup.com>
2019-10-01 13:47:03 +0200

    Fix setup of cheatsheets with new LULZ category for jp-style-faces

--- packages/cheat.el
+++ packages/cheat.el
@@ -1,3 +1,16 @@
 (require 'cheat)
+(setq cheat/categories '(
+     "JavaScript"
+     "Emacs"
+     "Markup"
+     "CLI"
+     "Org"
+     "HTML"
+     "C-like"
+     "Ruby"
+     "Python"
+     "LULZ"
+))
+                        
 (add-to-list 'cheat/sheets-folders "~/.emacs.d/cheatsheets/")
 (cheat/setup)
